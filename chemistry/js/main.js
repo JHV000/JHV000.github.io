5664a1b09c98c6ac26c8b8676d64d90576159f9e
JHV000 <49715621+JHV000@users.noreply.github.com>
2020-04-06 22:58:05 +0800

    化学品再回收平台

---
+++ chemistry/js/main.js
@@ -0,0 +1,9 @@
+function showG(){
+ document.getElementById("alertG").style.display= "block";
+ document.getElementById("mask").style.display= "block";
+}
+
+function out(){
+    document.getElementById("alertG").style.display= "none";
+ document.getElementById("mask").style.display= "none";
+}
