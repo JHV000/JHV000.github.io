41dd7b5a65076e8945cf73c4391b397ec9c8811e
JHV000 <49715621+JHV000@users.noreply.github.com>
2020-04-07 17:03:07 +0800

    修改细节 增加化学品信息

--- chemistry/js/main.js
+++ chemistry/js/main.js
@@ -2,8 +2,13 @@ function showG(){
  document.getElementById("alertG").style.display= "block";
  document.getElementById("mask").style.display= "block";
 }
-
+function showX(){
+    document.getElementById("alertX").style.display= "block";
+    document.getElementById("mask").style.display= "block";
+   }
+   
 function out(){
     document.getElementById("alertG").style.display= "none";
+    document.getElementById("alertX").style.display= "none";
  document.getElementById("mask").style.display= "none";
 }
